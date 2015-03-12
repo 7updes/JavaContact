745540a7b9454613ceb2bc4c0456d4fe535e6d82
Alex <7up-des@mail.ru>
2015-03-12 16:31:37 +0200

    EMPTY project(any actions start form here )

---
+++ src/main/java/contact/dao/impl/ContactDaoImpl.java
@@ -0,0 +1,35 @@
+package contact.dao.impl;
+
+import contact.dao.ContactDao;
+import contact.model.Contact;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Alex on 11.03.2015.
+ */
+public class ContactDaoImpl implements ContactDao{
+
+
+
+    @Override
+    public void addContact(Contact contact) {
+
+    }
+
+    @Override
+    public void deleteContact(Contact contact) {
+
+    }
+
+    @Override
+    public void addFriendShip(Contact c1, Contact c2) {
+
+    }
+
+    @Override
+    public void removeFriendShip(Contact c1, Contact c2) {
+
+    }
+}
